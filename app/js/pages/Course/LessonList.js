a1a3483991cbe39f08858827dad9177b236e6a7d
Jake Marsh <jakemmarsh@gmail.com>
2014-11-17 21:18:05 -0500

    rename LessonsList to LessonList

---
+++ app/js/pages/Course/LessonList.js
@@ -0,0 +1,43 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React         = require('react/addons');
+var _             = require('underscore');
+
+var LessonSnippet = require('../../components/LessonSnippet');
+
+var CourseLessonList = React.createClass({
+
+  getDefaultProps: function() {
+    return {
+      course: {}
+    };
+  },
+
+  renderLessons: function() {
+    var elements = null;
+
+    if ( !_.isEmpty(this.props.course) && this.props.course.lessons ) {
+      elements =  this.props.course.lessons.map(function(lesson, index) {
+        return (
+          <LessonSnippet course={this.props.course} lesson={lesson} key={index} />
+        );
+      }.bind(this));
+    }
+
+    return elements;
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.renderLessons()}
+      </div>
+    );
+  }
+
+});
+
+module.exports = React.createFactory(CourseLessonList);
