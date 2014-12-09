adf234fc804dfca15b3fba859a388d3cd6cd9d30
Jake Marsh <jakemmarsh@gmail.com>
2014-12-08 21:32:41 -0500

    Begin user dropdown and link to course creation in header

--- app/js/components/Header.js
+++ app/js/components/Header.js
@@ -4,14 +4,55 @@
 'use strict';
 
 var React = require('react/addons');
+var Link  = require('react-router').Link;
 
 var Header = React.createClass({
+
+  propTypes: {
+    currentUser: React.PropTypes.object.isRequired
+  },
+
+  getDefaultProps: function() {
+    return {
+      currentUser: {}
+    };
+  },
+
+  renderCreateCourseButton: function() {
+    var element = null;
+
+    if ( this.props.currentUser.type === 'instructor' ) {
+      element = (
+        <Link to="CreateCourse" className="create-course-button">
+          <i className="fa fa-plus" />
+        </Link>
+      );
+    }
+
+    return element;
+  },
 
   render: function() {
     return (
       <header>
-        <img className="logo" src="../images/logo.png" alt="acadeME logo" />
+
+        <div className="left-container">
+          {this.renderCreateCourseButton()}
+        </div>
+
+        <div className="logo-container">
+          <img className="logo" src="../images/logo.png" alt="acadeME logo" />
+        </div>
+
+        <div className="right-container">
+          <div className="dropdown-container">
+            {this.props.currentUser.name}
+            <i className="fa fa-caret-down" />
+          </div>
+        </div>
+
         <div className="shadow" />
+
       </header>
     );
   }
