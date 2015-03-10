9835454a4af6173017ab1fcfc439a0421abb06ed
Jake Marsh <jakemmarsh@gmail.com>
2015-03-09 20:34:02 -0400

    small fixes

--- app/js/pages/CoursePage.js
+++ app/js/pages/CoursePage.js
@@ -6,12 +6,16 @@
 var React         = require('react');
 var _             = require('lodash');
 var RouteHandler  = React.createFactory(require('react-router').RouteHandler);
+var Link          = React.createFactory(require('react-router').Link);
 
 var DocumentTitle = require('../components/DocumentTitle');
+var ListLink      = require('../components/ListLink');
 var CourseActions = require('../actions/CourseActions');
 var TopMenu       = require('../components/TopMenu');
 
 var CoursePage = React.createClass({
+
+  mixins: [React.addons.LinkedStateMixin],
 
   propTypes: {
     currentUser: React.PropTypes.object.isRequired,
@@ -25,6 +29,12 @@ var CoursePage = React.createClass({
     };
   },
 
+  getInitialState: function() {
+    return {
+      query: this.props.query.q ? this.props.query.q.replace(/(\+)|(%20)/gi, ' ') : ''
+    };
+  },
+
   componentWillReceiveProps: function(nextProps) {
     if ( _.isEmpty(this.props.course) || this.props.params.courseId !== nextProps.params.courseId ) {
       CourseActions.openCourse(nextProps.params.courseId.toString());
@@ -35,13 +45,54 @@ var CoursePage = React.createClass({
     CourseActions.openCourse(this.props.params.courseId.toString());
   },
 
+  submitOnEnter: function(evt) {
+    var keyCode = evt.keyCode || evt.which;
+
+    if ( (keyCode === '13' || keyCode === 13) && !_.isEmpty(this.props.course) ) {
+      this.transitionTo('CourseSearch', { courseId: this.props.course.id }, { q: this.state.query });
+    }
+  },
+
+  renderCreateButton: function() {
+    var element = null;
+
+    if ( !_.isEmpty(this.props.course) && this.props.course.instructor.id === this.props.currentUser.id ) {
+      element = (
+        <li className="create-lesson-button">
+          <i className="fa fa-plus" />
+          <Link to="CreateLesson" params={{ courseId: this.props.course.id }} />
+        </li>
+      );
+    }
+
+    return element;
+  },
+
   render: function() {
     return (
       <section className="course-page">
 
         <DocumentTitle title={this.props.course ? this.props.course.title : ''} />
 
-        <TopMenu currentUser={this.props.currentUser} course={this.props.course} query={this.props.query.q} />
+        <TopMenu>
+          <li className="search-container">
+            <i className="fa fa-search" />
+            <input type="text"
+                   className="search"
+                   placeholder="Search in course..."
+                   valueLink={this.linkState('query')}
+                   onKeyPress={this.submitOnEnter} />
+          </li>
+          <ListLink to="Course" params={ {courseId: this.props.course.id || 0} }>
+            <i className="fa fa-book" />
+            Lessons
+          </ListLink>
+          <ListLink to="CourseChat" params={ {courseId: this.props.course.id || 0} }>
+            <i className="fa fa-comments" />
+            Chat
+          </ListLink>
+          {this.renderCreateButton()}
+        </TopMenu>
 
         <RouteHandler params={this.props.params}
                       query={this.props.query}
